d9fe15bb3a6131cfa621396f3380d604f3aa6307
mxrsel <marsel14082006@gmail.com>
2024-11-13 13:16:32 +0600

    added props for getting data types and added it to jsx for displaying data

--- src/components/ShowItem/ShowItem.tsx
+++ src/components/ShowItem/ShowItem.tsx
@@ -1,8 +1,19 @@
+import React from 'react';
+import {IShow} from "../../types.ts";
 
-const ShowItem = () => {
+interface IShowItemProps {
+    show: IShow
+}
+
+const ShowItem : React.FC<IShowItemProps>= ({show}) => {
     return (
-        <div>
-
+        <div key={show.id}>
+            <img
+                src={show.image}
+                alt={show.name}
+            />
+            <h1>{show.name}</h1>
+            <p>{show.summary}</p>
         </div>
     );
 };
